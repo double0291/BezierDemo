4b48fc1a04e3122cb6cc7898ab3475cadc9b9ab6
double0291 <double0291@126.com>
2015-10-31 16:46:24 +0800

    XXX

--- BezierDemo/src/com/bezierdemo/ui/Fragment2.java
+++ BezierDemo/src/com/bezierdemo/ui/Fragment2.java
@@ -6,12 +6,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Fragment2 extends Fragment implements OnClickListener {
+    TextView mRedPoint;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_1, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_2, container, false);
+
+        mRedPoint = (TextView) rootView.findViewById(R.id.red_point);
 
         // Bundle args = getArguments();
         return rootView;
@@ -20,6 +24,8 @@ public class Fragment2 extends Fragment implements OnClickListener {
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+        case R.id.red_point:
+            break;
 
         default:
             break;
--- BezierDemo/src/com/bezierdemo/ui/Fragment3.java
+++ BezierDemo/src/com/bezierdemo/ui/Fragment3.java
@@ -11,7 +11,7 @@ public class Fragment3 extends Fragment implements OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_1, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_3, container, false);
 
         // Bundle args = getArguments();
         return rootView;
